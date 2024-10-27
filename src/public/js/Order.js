7de7a912286a4928a76d9fa711c15b36f7ccd84b
Dang Cao Hau <caohauhau9@gmail.com>
2024-10-27 17:04:32 +0700

    cập nhật các chức năng của form điền mess

--- src/public/js/Order.js
+++ src/public/js/Order.js
@@ -9,7 +9,7 @@
   js.src = "//connect.facebook.net/en_US/messenger.Extensions.js";
   fjs.parentNode.insertBefore(js, fjs);
 })(document, "script", "Messenger");
-
+alert("hello");
 window.extAsyncInit = function () {
   // the Messenger Extensions JS SDK is done loading
   MessengerExtensions.getContext(
