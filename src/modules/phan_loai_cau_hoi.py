c00bd9ac6e7106c153e2cfd440870d67472085f0
cakienx4 <cakien197@gmail.com>
2024-11-03 20:56:01 +0700

    update file plch.py

--- src/modules/phan_loai_cau_hoi.py
+++ src/modules/phan_loai_cau_hoi.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import csv
 import os
+import yaml
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
@@ -25,7 +26,19 @@ y_pred = model.predict(X_test)
 accuracy = model.score(X_test, y_test)
 print(f"Độ chính xác: {accuracy * 100:.2f}%")
 
-new_text = ["cái vest này giá bao nhiêu"]
+new_text = ["hello"]
 new_vector = vectorizer.transform(new_text)
 prediction = model.predict(new_vector)
 print(f"Dự đoán: {prediction[0]}")
+
+def load_yaml_by_label(label):
+    yaml_path = os.path.join(os.path.dirname(__file__), '..', 'data', 'cau_tra_loi', f"{label}.yml")
+    if os.path.exists(yaml_path):
+        with open(yaml_path, 'r', encoding='utf-8') as file:
+            yaml_data = yaml.safe_load(file)
+            return yaml_data
+    else:
+        print(f"No YAML file found for label: {label}")
+        return None
+yaml_content = load_yaml_by_label(prediction[0])
+print("Dữ liệu từ file YAML:", yaml_content)
