fadad5a30d9191fc551306c63575debffcf1d1d8
cakienx4 <cakien197@gmail.com>
2024-11-04 22:58:16 +0700

    cập nhật phân loại câu hỏi và data

--- src/modules/phan_loai_cau_hoi.py
+++ src/modules/phan_loai_cau_hoi.py
@@ -1,33 +1,40 @@
 import pandas as pd
 import numpy as np
-import csv
 import os
 import yaml
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
 
-csv_path = os.path.join(os.path.dirname(__file__), '..','data', 'cau_hoi', 'pl_cau_hoi.csv')
-data_cau_hoi = pd.read_csv(csv_path)
+def phan_loai_cau_hoi(new_text):
+    csv_path = os.path.join(os.path.dirname(__file__), '..','data', 'cau_hoi', 'pl_cau_hoi.csv')
+    data_cau_hoi = pd.read_csv(csv_path)
 
-data_list = np.array(data_cau_hoi['data'])
-label_list = np.array(data_cau_hoi['label'])
+    data_list = np.array(data_cau_hoi['data'])
+    label_list = np.array(data_cau_hoi['label'])
 
-vectorizer = TfidfVectorizer()
-X = vectorizer.fit_transform(data_list)
+    vectorizer = TfidfVectorizer()
+    X = vectorizer.fit_transform(data_list)
 
-X_train, X_test, y_train, y_test = train_test_split(
-    X, label_list, test_size=0.2, random_state=22)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, label_list, test_size=0.2, random_state=22)
 
-model = SVC(kernel="linear")
-model.fit(X_train, y_train)
-y_pred = model.predict(X_test)
+    model = SVC(kernel="linear")
+    model.fit(X_train, y_train)
 
-accuracy = model.score(X_test, y_test)
-print(f"Độ chính xác: {accuracy * 100:.2f}%")
+    #kiểm tra độ chính xác
+    accuracy = model.score(X_test, y_test)
+    print(f"Độ chính xác: {accuracy * 100:.2f}%")
+    new_vector = vectorizer.transform([new_text])
+    prediction = model.predict(new_vector)[0]
+    with open('../data/cau_tra_loi/tu_van.yml','r', encoding='utf-8') as file_yml:
+        cau_tra_loi = yaml.safe_load(file_yml)
+        print(cau_tra_loi[prediction])
+    return cau_tra_loi[prediction]
 
-new_text = ["hello"]
-new_vector = vectorizer.transform(new_text)
-prediction = model.predict(new_vector)
-print(f"Dự đoán: {prediction[0]}")
+# #ví dụ dự đoán
+# new_text = ["áo đẹp nhất "]
+# du_doan = phan_loai_cau_hoi(new_text)
+# print(du_doan)
 
+phan_loai_cau_hoi("Ưu đãi")
