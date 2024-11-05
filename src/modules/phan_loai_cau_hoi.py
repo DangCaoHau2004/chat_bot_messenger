bd265606220d7efd4a57f13a9120fa76e100cbe4
cakienx4 <cakien197@gmail.com>
2024-11-05 19:54:25 +0700

    update file plch.py

--- src/modules/phan_loai_cau_hoi.py
+++ src/modules/phan_loai_cau_hoi.py
@@ -34,6 +34,23 @@ def phan_loai_cau_hoi(new_text):
     if prediction in cau_tra_loi:
         response = cau_tra_loi[prediction]
     else:
+        csv_path = os.path.join(os.path.dirname(__file__), '..','data', 'cau_hoi', 'pl_san_pham.csv')
+        data_cau_hoi = pd.read_csv(csv_path)
+
+        data_list = np.array(data_cau_hoi['data'])
+        label_list = np.array(data_cau_hoi['label'])
+
+        vectorizer = TfidfVectorizer()
+        X = vectorizer.fit_transform(data_list)
+
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, label_list, test_size=0.2, random_state=22)
+
+        model = SVC(kernel="linear")
+        model.fit(X_train, y_train)
+
+        new_vector = vectorizer.transform([new_text])
+        prediction = model.predict(new_vector)[0]
         with open('../data/cau_tra_loi/san_pham.yml','r', encoding='utf-8') as file_sanpham_yml:
             cau_tra_loi = yaml.safe_load(file_sanpham_yml)
             response = cau_tra_loi[prediction]
@@ -45,4 +62,4 @@ def phan_loai_cau_hoi(new_text):
 # du_doan = phan_loai_cau_hoi(new_text)
 # print(du_doan)
 
-phan_loai_cau_hoi("sản phẩm")
+phan_loai_cau_hoi("vest")
